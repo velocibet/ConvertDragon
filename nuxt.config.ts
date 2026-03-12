402e7cf2ea56766f3e5d19d97c645ae4213e4b69
velocibet <honoka4h@gmail.com>
2026-03-12 23:44:07 +0900

    add: google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15", // 현재 날짜에 맞춤
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   
-  // 모듈만 딱 선언 (Nuxt가 알아서 Tailwind v3를 로드합니다)
   modules: [
     '@nuxtjs/tailwindcss',
     "@nuxtjs/sitemap",
     "@nuxtjs/robots",
+    'nuxt-gtag'
   ],
 
   site: {
@@ -16,6 +16,9 @@ export default defineNuxtConfig({
     name: 'ConvertDragon'
   },
 
-  // css 경로는 유지하되, 파일이 없다면 일단 주석 처리해도 됩니다.
+  gtag: {
+    id: 'G-VD30H2Q9MM' 
+  }
+
   // css: ['./app/assets/css/main.css'],
 });
